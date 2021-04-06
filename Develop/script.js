c644da5404075882b79f779626a14109617ca234
Joseph Greenwell <joeygreenwell@ymail.com>
2021-04-05 22:54:35 -0500

    linking jQuery and adding button function

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,14 @@
-var city="Austin";
-fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=daa1ac9179b64017705840dffa558075`)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
+function search() {
+    var city = $(this).parent().siblings(".searchCity").val();
+    console.log(city);
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=daa1ac9179b64017705840dffa558075`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        });
 
+};
+
+$(".btn").on("click", search);
