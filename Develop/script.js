4deed7c2e361acf0b2f2fe766861ca566ac41749
Joseph Greenwell <joeygreenwell@ymail.com>
2021-04-06 01:04:08 -0500

    more work on functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,7 @@
+var previousCities=[];
+var todayDate=moment().format("MMMM Do, YYYY");
+
+// a fetch that takes in a city from input as a search query
 function search() {
     var city = $(this).parent().siblings(".searchCity").val();
     console.log(city);
@@ -9,5 +13,16 @@ function search() {
             console.log(data);
         });
 };
+
+// puts fetched data into the main forcast
+function mainForecast(){
+
+};
+
+// function updateCards(){
+//     for (var i = 0; i < 4; i++){
+//         $(".forecast").add()
+//     }
+// };
 
 $(".btn").on("click", search);
