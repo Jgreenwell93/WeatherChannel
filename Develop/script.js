dcbc3ac9cbc7e2c00250c48ac09b04ff10d88bdb
Joseph Greenwell <joeygreenwell@ymail.com>
2021-04-06 20:19:41 -0500

    reworking a function

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,6 @@
-var previousCities=[];
+// var previousCities=[];
 var todayDate=moment().format("MMMM Do, YYYY");
+
 
 // a fetch that takes in a city from input as a search query
 function search() {
@@ -11,13 +12,34 @@ function search() {
         })
         .then(function (data) {
             console.log(data);
-        });
+            var lat=data['city']['coord']['lat'];
+            var lon=data['city']['coord']['lon'];
+
+        })
+
+          function oneSearch(lat,lon){
+              fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}}&appid=daa1ac9179b64017705840dffa558075`)
+              then(function(response){
+                  return response.json();
+              })
+              .then(function(data){
+                  console.log(data);
+              })
+              .catch(function (err) {
+                console.log(err);
+              });
+          };
+
 };
+
+
+
+
 
 // puts fetched data into the main forcast
-function mainForecast(){
+// function mainForecast(){
 
-};
+// };
 
 // function updateCards(){
 //     for (var i = 0; i < 4; i++){
