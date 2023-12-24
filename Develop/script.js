39e970900e7aca6ab55ee608368ba468b9efb533
Joseph Greenwell <joeygreenwell@ymail.com>
2023-12-24 12:19:14 -0600

    nested all forcast information into a single div to add padding, removed flex space between due to breaking with padding. will have to do something else and probably make each forcast card the same size

--- Develop/script.js
+++ Develop/script.js
@@ -148,12 +148,14 @@ function updateCards(data) {
         <div class="forecastCards card">
         <h class="dates forecast-card-info">${moment().add(i, 'days').format('dddd M/D')}</h>
 <img src='http://openweathermap.org/img/wn/${ficons}@2x.png' alt="weathericon">
+<div class="forecast-card-info"
 <p class="futureWeath forecast-card-info">${capitalizedfWeath}</p>
-<p class="forecast-card-info">Temperature High: ${ftempsh}°F</p>
-<p class="forecast-card-info">Temperature Low: ${ftempsl}°F</p>
-<p class="forecast-card-info">Humidity: ${fhumid}%</p>
-<p class="forecast-card-info">Wind Speed: ${fwind}MPH</p>
-<p class="forecast-card-info">UV Index: ${fuv}</p>
+<p>Temperature High: ${ftempsh}°F</p>
+<p>Temperature Low: ${ftempsl}°F</p>
+<p>Humidity: ${fhumid}%</p>
+<p>Wind Speed: ${fwind}MPH</p>
+<p>UV Index: ${fuv}</p>
+</div>
         </div>
         `)
     }
